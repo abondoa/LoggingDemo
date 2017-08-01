4e7122da76fc7a179708cbf946728782215773eb
Andrew Cook <codingwithcookie@outlook.com>
2017-07-31 23:26:36 -0700

    Added logging to controller actions.

--- LoggingDemo/Controllers/HomeController.cs
+++ LoggingDemo/Controllers/HomeController.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using NLog;
 using System.Web.Mvc;
 
 namespace LoggingDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private static ILogger _log => LogManager.GetCurrentClassLogger();
+
         public ActionResult Index()
         {
+            _log.Debug("Entered the Home Controller Index Action.");
+
             return View();
         }
 
         public ActionResult About()
         {
+            _log.Debug("Entered the Home Controller About Action.");
+
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -22,6 +25,8 @@ namespace LoggingDemo.Controllers
 
         public ActionResult Contact()
         {
+            _log.Debug("Entered the Home Controller Contact Action.");
+
             ViewBag.Message = "Your contact page.";
 
             return View();
